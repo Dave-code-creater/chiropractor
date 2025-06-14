67b956c62f14be42e183401f8dfba261c24ddd04
Dave <80132827+Dave-code-creater@users.noreply.github.com>
2025-06-14 11:13:39 -0400

    test: replace sinon with chai

--- blog-service/test/posts.test.js
+++ blog-service/test/posts.test.js
@@ -1,38 +1,41 @@
 const request = require('supertest');
-const sinon = require('sinon');
+const chai = require('chai');
+const spies = require('chai-spies');
 const PostService = require('../src/services/post.service.js');
 const app = require('../src/index.js');
-const { strict: assert } = require('assert');
 const jwt = require('jsonwebtoken');
 
+chai.use(spies);
+const { expect } = chai;
+
 describe('blog-service post endpoints', () => {
-  afterEach(() => sinon.restore());
+  afterEach(() => chai.spy.restore());
 
   it('creates post', async () => {
-    sinon.stub(jwt, 'verify').returns({ sub: 1 });
-    sinon.stub(PostService, 'create').resolves({ _id: '1' });
+    chai.spy.on(jwt, 'verify', () => ({ sub: 1 }));
+    chai.spy.on(PostService, 'create', () => Promise.resolve({ _id: '1' }));
     const res = await request(app)
       .post('/posts')
       .set('authorization', 'Bearer token')
       .send({ title: 't', body: 'b', author: 1 });
-    assert.equal(res.status, 201);
+    expect(res.status).to.equal(201);
   });
 
   it('gets post', async () => {
-    sinon.stub(jwt, 'verify').returns({ sub: 1 });
-    sinon.stub(PostService, 'getById').resolves({ _id: '1' });
+    chai.spy.on(jwt, 'verify', () => ({ sub: 1 }));
+    chai.spy.on(PostService, 'getById', () => Promise.resolve({ _id: '1' }));
     const res = await request(app)
       .get('/posts/1')
       .set('authorization', 'Bearer token');
-    assert.equal(res.status, 200);
+    expect(res.status).to.equal(200);
   });
 
   it('lists posts by user', async () => {
-    sinon.stub(jwt, 'verify').returns({ sub: 1 });
-    sinon.stub(PostService, 'listByUser').resolves([{ _id: '1' }]);
+    chai.spy.on(jwt, 'verify', () => ({ sub: 1 }));
+    chai.spy.on(PostService, 'listByUser', () => Promise.resolve([{ _id: '1' }]));
     const res = await request(app)
       .get('/users/1/posts')
       .set('authorization', 'Bearer token');
-    assert.equal(res.status, 200);
+    expect(res.status).to.equal(200);
   });
 });
