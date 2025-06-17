9937968a7c5a662dbae8ef1710359d8f0838d63b
Dave-code-creater <dat981dat@gmail.com>
2025-06-17 19:51:58 -0400

    add test doctor

--- auth-service/migrations/002_create_doctor.sql
+++ auth-service/migrations/002_create_doctor.sql
@@ -1,4 +1,7 @@
+-- Username: 'doctor'
+-- Password: 'Oces2023@'
+
 INSERT INTO users 
 (email, username, password_hash, role, phone_number, is_verified, phone_verified, status, last_login_at, created_at, updated_at)
 VALUES 
-('doctor@gmail.com', 'doctor', '$2a$10$ANjR3bLxsu4S998RZgs6Ve8ItK7gBcoe2HKy5Bf2Hbc0jaoVmXJ.S', 'doctor', '1234567890', TRUE, TRUE, 'active', NOW(), NOW(), NOW());
+('doctor@gmail.com', 'doctor', '$2y$10$f2KnQsrCZQ/LRJeEy9kbjeuRBqmSmWwAqfC66BgyE5hf13EIMoixq', 'doctor', '1234567890', TRUE, TRUE, 'active', NOW(), NOW(), NOW());
--- auth-service/src/validators/access.js
+++ auth-service/src/validators/access.js
@@ -9,7 +9,11 @@ const signUpValidator = joi.object({
     .message('password must be at least 8 chars and contain only letters, numbers or symbols')
     .required(),
   first_name: joi.string().alphanum().min(2).required(),
+  phone_number: joi.string().pattern(/^\+?[1-9]\d{1,14}$/),
   last_name: joi.string().alphanum().min(2).required(),
+  confirm_password: joi.string().valid(joi.ref('password')).required().messages({
+    'any.only': 'confirm_password must match password'
+  })
 });
 
 const signInValidator = joi.object({
